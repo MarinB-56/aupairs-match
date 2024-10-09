97eec3ccfc2eb8986ed1262fa55765c7f5c2efc9
Mathieu Fernandez <mathieu.fernandez.v@gmail.com>
2024-10-09 20:09:13 +0200

    Setting up routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :users, only: [:index, :show]
+
+  get "my_favorites", to: "favorites#my_favorites"
+  get "my_matches", to: "matches#my_matches"
+
+  resources :favorites, only: [:create, :destroy]
+  resources :matches, only: [:create, :destroy, :update]
 
   # Defines the root path route ("/")
   # root "posts#index"
