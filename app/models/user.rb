261e490bb8c0efe3d5259ade9567dea49525c179
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2024-10-10 19:06:02 +0200

    Create User model
    Use 'rails generate devise User' to create the User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :favorites_given, class_name: 'Favorite', foreign_key: :favoriting_user_id
   has_many :favorites_received, class_name: 'Favorite', foreign_key: :favorited_user_id
   has_many :matches_given, class_name: 'Favorite', foreign_key: :initiated_by_id
