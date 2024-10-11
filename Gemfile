3ba18fe97a0a68783480764bae6a1759a9cac433
Mathieu Fernandez <mathieu.fernandez.v@gmail.com>
2024-10-11 21:25:02 +0200

    Adding JqueryUI

--- Gemfile
+++ Gemfile
@@ -34,6 +34,9 @@ gem "jbuilder"
 # Faker pour seeder la base de données
 gem "faker"
 
+# JQuery pour gérer des éléments dynamiques
+gem "jquery-ui-rails"
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,35 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user! # Assure que seul un utilisateur connecté accède à l'index
+  before_action :authenticate_user!
 
   def index
-    @roles = User.roles.keys
-    @users = if current_user.aupair?
-               User.family # Si l'utilisateur est au_pair, on affiche les familles
+    # Si l'utilisateur est une family, on affiche les au_pair, sinon on affiche les familles
+    @users = if current_user.family?
+               User.aupair
              else
-               User.aupair # Sinon, on affiche les au_pairs
+               User.family
              end
 
-    # Si un paramètre de rôle est passé dans l'URL, on filtre par ce rôle
-    if params[:role].present?
-      @users = @users.where(role: params[:role])
+    # Récupérer les bornes min et max du nombre d'enfants parmi les familles
+    @min_children = User.family.minimum(:number_of_children)
+    @max_children = User.family.maximum(:number_of_children)
+
+    # Application des filtres
+    if params[:filters].present?
+      if current_user.family?
+        # Filtres pour au_pair
+        @users = @users.where(nationality: params[:filters][:nationality]) if params[:filters][:nationality].present?
+        @users = @users.where(gender: params[:filters][:gender]) if params[:filters][:gender].present?
+      else
+        # Filtres pour familles
+        @users = @users.where(nationality: params[:filters][:nationality]) if params[:filters][:nationality].present?
+
+        # Filtre sur le nombre d'enfants avec un range
+        if params[:filters][:min_number_of_children].present? && params[:filters][:max_number_of_children].present?
+          min_children = params[:filters][:min_number_of_children].to_i
+          max_children = params[:filters][:max_number_of_children].to_i
+          @users = @users.where(number_of_children: min_children..max_children)
+        end
+      end
     end
 
     respond_to do |format|
