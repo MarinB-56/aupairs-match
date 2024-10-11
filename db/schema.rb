2df7ceb5f5e66627545121cb66f2adc76034e120
Mathieu Fernandez <mathieu.fernandez.v@gmail.com>
2024-10-11 20:50:58 +0200

    Improved seeding, new default avatar in navbar, logout button

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_10_170441) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_11_164238) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -96,6 +96,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_10_170441) do
     t.integer "number_of_children"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0, null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,3 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
 require 'faker'
 require 'open-uri'
 
@@ -19,6 +9,7 @@ puts "Création des utilisateurs (au pairs et familles)..."
 # Array of real photo filenames
 photo_filenames = ['ap_1.jpeg', 'ap_2.jpeg', 'ap_3.jpeg', 'ap_4.jpeg', 'ap_5.jpeg', 'ap_6.jpeg', 'ap_7.jpeg', 'ap_8.jpeg', 'ap_9.jpeg']
 
+puts "Création des au pairs"
 # Créer 8 au pairs avec des photos random
 8.times do |i|
   user = User.new(
@@ -29,22 +20,23 @@ photo_filenames = ['ap_1.jpeg', 'ap_2.jpeg', 'ap_3.jpeg', 'ap_4.jpeg', 'ap_5.jpe
     nationality: Faker::Address.country,
     gender: "Femme",
     location: Faker::Address.city,
-    role: "aupair"
+    role: "aupair",
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 8)
   )
-  # Adding email and password
-  user.email = "#{user.first_name}@gmail.com"
-  user.password = 'password'
 
-  # Saving the new user to database
-  user.save
+  if user.save
+    # Attacher une photo locale à l'utilisateur
+    file_path = Rails.root.join('app', 'assets', 'images', 'aupairs_seeds', photo_filenames[i % photo_filenames.length])
+    user.photo.attach(io: File.open(file_path), filename: photo_filenames[i % photo_filenames.length], content_type: 'image/jpeg')
 
-  # Attach a real local photo from assets
-  file_path = Rails.root.join('app', 'assets', 'images', 'aupairs_seeds', photo_filenames[i % photo_filenames.length])
-  user.photo.attach(io: File.open(file_path), filename: photo_filenames[i % photo_filenames.length], content_type: 'image/jpeg')
-
-  user.save ? (puts "#{user.first_name} - Au pair #{i + 1} créée !") : (puts "Au pair non créée")
+    puts "#{user.first_name} - Au pair #{i + 1} créée !"
+  else
+    puts "Erreur lors de la création de l'au pair #{i + 1}"
+  end
 end
 
+puts "Création des familles"
 # Créer 10 familles avec des données random
 10.times do |i|
   user = User.new(
@@ -53,14 +45,16 @@ end
     nationality: Faker::Nation.nationality,
     location: Faker::Address.city,
     number_of_children: rand(1..4),
-    role: "family"
+    role: "family",
+    email: Faker::Internet.email,           # Utilisation de Faker pour un email aléatoire
+    password: Faker::Internet.password(min_length: 8) # Mot de passe aléatoire avec Faker
   )
-  # Adding email and password
-  user.email = "#{user.first_name}@gmail.com"
-  user.password = 'password'
 
-  # Saving the new user to database
-  user.save ? (puts "#{user.first_name} - Famille #{i + 1} créée") : (puts "Famille non créée")
+  if user.save
+    puts "#{user.last_name} - Famille #{i + 1} créée !"
+  else
+    puts "Erreur lors de la création de la famille #{i + 1}"
+  end
 end
 
 puts "Création terminée !"
