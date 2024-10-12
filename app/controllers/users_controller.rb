fb1ea1e764c6b646f22697ce44b51ba5ec49257e
Mathieu Fernandez <mathieu.fernandez.v@gmail.com>
2024-10-12 07:56:14 +0200

    Modifies seed file to generate fake users with API

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    # Définir les utilisateurs à afficher en fonction du rôle actuel (family ou au pair)
     @users = if current_user.family?
                User.aupair
              else
@@ -14,20 +15,21 @@ class UsersController < ApplicationController
 
     # Application des filtres
     if params[:filters].present?
-      # Filtrer par nationalité
-      if params[:filters][:nationality].present?
-        @users = @users.where(nationality: params[:filters][:nationality]) unless params[:filters][:nationality].nil?
+
+      # Filtre de nationalité (pour les au pairs ou les familles, selon le rôle de l'utilisateur)
+      if params[:filters][:nationality].present? && params[:filters][:nationality] != ""
+        @users = @users.where(nationality: params[:filters][:nationality])
       end
 
-      # Filtrer par nombre d'enfants maximum
-      if params[:filters][:max_number_of_children].present?
+      # Filtre de genre (appliqué uniquement si l'utilisateur est une famille recherchant des au pairs)
+      if current_user.family? && params[:filters][:gender].present? && params[:filters][:gender] != ""
+        @users = @users.where(gender: params[:filters][:gender])
+      end
+
+      # Filtrer par nombre d'enfants maximum (uniquement si l'utilisateur est un au pair recherchant des familles)
+      if current_user.aupair? && params[:filters][:max_number_of_children].present?
         @users = @users.where("number_of_children <= ?", params[:filters][:max_number_of_children].to_i)
       end
-    end
-
-    respond_to do |format|
-      format.html
-      format.js
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,59 +1,97 @@
 require 'faker'
 require 'open-uri'
+require 'net/http'
+require 'json'
+
+# On définit une fonction qui appelle l'API randomuser pour récupérer un lot d'utilisateurs
+def fetch_random_users(batch_size: 1000)
+  url = URI("https://randomuser.me/api/?nat=us,gb,fr,es,de&inc=gender,name,location,email,login,dob,nat,picture&results=#{batch_size}&noinfo")
+  response = Net::HTTP.get(url)
+  JSON.parse(response)['results']
+end
+
+# On filtre les utilisateurs pour ne garder que ceux dans la tranche d'âge
+def filter_users_by_age(users, age_min: 18, age_max: 30)
+  users.select do |user|
+    age = user['dob']['age']
+    age >= age_min && age <= age_max
+  end
+end
 
 puts "Nettoyage de la base de données..."
 User.destroy_all
 
 puts "Création des utilisateurs (au pairs et familles)..."
 
-# Array of real photo filenames
-photo_filenames = ['ap_1.jpeg', 'ap_2.jpeg', 'ap_3.jpeg', 'ap_4.jpeg', 'ap_5.jpeg', 'ap_6.jpeg', 'ap_7.jpeg', 'ap_8.jpeg', 'ap_9.jpeg']
-
 puts "Création des au pairs"
-# Créer 8 au pairs avec des photos random
-8.times do |i|
+
+# Récupérer un lot d'utilisateurs depuis l'API
+random_users = fetch_random_users(batch_size: 1000)
+# Filtrer les utilisateurs par tranche d'âge
+filtered_users = filter_users_by_age(random_users, age_min: 18, age_max: 30)
+
+# Si moins de 10 utilisateurs dans la tranche d'âge, afficher un message
+if filtered_users.size < 10
+  puts "Attention : Moins de 10 utilisateurs disponibles dans la tranche d'âge sélectionnée."
+end
+
+# Créer 10 au pairs avec les utilisateurs filtrés
+filtered_users.first(10).each_with_index do |random_user, i|
+  # Les données de localisations sont reconstruites à partir de la réponse de l'API
+  location = random_user['location']
+  full_location = "#{location['street']['number']} #{location['street']['name']}, #{location['city']}, #{location['state']}, #{location['country']}"
+
+  # Parse la date d'anniversaire pour nous assurer que le format est correct
+  birth_date = Date.parse(random_user['dob']['date'])
+
   user = User.new(
-    first_name: Faker::Name.female_first_name,
-    last_name: Faker::Name.last_name,
-    birth_date: Faker::Date.birthday(min_age: 18, max_age: 30),
+    first_name: random_user['name']['first'],
+    last_name: random_user['name']['last'],
+    birth_date: birth_date,
     description: Faker::Lorem.sentence(word_count: 15),
-    nationality: Faker::Address.country,
-    gender: "Femme",
-    location: Faker::Address.city,
+    nationality: random_user['nat'],
+    gender: random_user['gender'],
+    location: full_location,
     role: "aupair",
-    email: Faker::Internet.email,
-    password: Faker::Internet.password(min_length: 8)
+    email: random_user['email'],
+    password: Faker::Internet.password(min_length: 8) # Utilisation de Faker pour générer un mot de passe
   )
 
   if user.save
-    # Attacher une photo locale à l'utilisateur
-    file_path = Rails.root.join('app', 'assets', 'images', 'aupairs_seeds', photo_filenames[i % photo_filenames.length])
-    user.photo.attach(io: File.open(file_path), filename: photo_filenames[i % photo_filenames.length], content_type: 'image/jpeg')
-
-    puts "#{user.first_name} - Au pair #{i + 1} créée !"
+    # Attacher une photo depuis l'API randomuser
+    user.photo.attach(io: URI.open(random_user['picture']['large']), filename: "aupair_#{i + 1}.jpg", content_type: 'image/jpeg')
+    puts "#{user.first_name} #{user.last_name} - Au pair #{i + 1} créée avec succès!"
   else
-    puts "Erreur lors de la création de l'au pair #{i + 1}"
+    puts "Erreur lors de la création de l'au pair #{i + 1}: #{user.errors.full_messages.join(", ")}"
   end
 end
 
 puts "Création des familles"
-# Créer 10 familles avec des données random
+
+# Créer 10 familles avec des données aléatoires depuis l'API randomuser
 10.times do |i|
+  # On prend un user depuis l'API random user pour chaque famille
+  random_user = fetch_random_users(batch_size: 1).first
+
+  # Les données de localisations sont reconstruites à partir de la réponse de l'API
+  location = random_user['location']
+  full_location = "#{location['street']['number']} #{location['street']['name']}, #{location['city']}, #{location['state']}, #{location['country']}"
+
   user = User.new(
-    last_name: Faker::Name.last_name,
-    description: "Nous sommes une famille à la recherche d'une au pair pour nos enfants.",
-    nationality: Faker::Nation.nationality,
-    location: Faker::Address.city,
-    number_of_children: rand(1..4),
+    last_name: random_user['name']['last'],
+    description: Faker::Lorem.sentence(word_count: 15),
+    nationality: random_user['nat'],
+    location: full_location,
+    number_of_children: rand(1..5),  # Nombre d'enfants généré avec Faker
     role: "family",
-    email: Faker::Internet.email,           # Utilisation de Faker pour un email aléatoire
-    password: Faker::Internet.password(min_length: 8) # Mot de passe aléatoire avec Faker
+    email: random_user['email'],
+    password: Faker::Internet.password(min_length: 8)  # Utilisation de Faker pour générer un mot de passe
   )
 
   if user.save
-    puts "#{user.last_name} - Famille #{i + 1} créée !"
+    puts "#{user.last_name} - Famille #{i + 1} créée avec succès!"
   else
-    puts "Erreur lors de la création de la famille #{i + 1}"
+    puts "Erreur lors de la création de la famille #{i + 1}: #{user.errors.full_messages.join(", ")}"
   end
 end
 
