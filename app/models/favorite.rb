16095b8a3d198375eecfadb104b91af402f06067
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2024-10-17 19:23:32 +0200

    Add uniqueness validation to favorite

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,6 @@
 class Favorite < ApplicationRecord
   belongs_to :favoriting_user, class_name: 'User'
   belongs_to :favorited_user, class_name: 'User'
+
+  validates :favoriting_user, uniqueness: { scope: :favorited_user }
 end
