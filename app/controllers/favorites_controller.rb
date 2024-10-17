7c638e677613829564189a9006457ebe36ece130
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2024-10-17 19:27:22 +0200

    Add 'add favorite' action to user's index
    Users can now add an aupair/family to its favorites list.
    Users cannot add an aupair/family twice (= if the aupair is already in the fav list, it is not possible to do it again.)

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,16 @@
 class FavoritesController < ApplicationController
+  def create
+    visited_user = User.find(params[:visited_user])
+    visiting_user = User.find(params[:visiting_user])
+
+    favorite = Favorite.new
+    favorite.favoriting_user = visiting_user
+    favorite.favorited_user = visited_user
+
+    if favorite.save
+      redirect_to users_path
+    else
+      redirect_to users_path, status: :unprocessable_entity
+    end
+  end
 end
