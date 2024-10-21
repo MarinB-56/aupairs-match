0e35b5317564f45154fd5244f348267a3f7cb5b2
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2024-10-21 21:30:46 +0200

    Fix root bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'users#index'
 
   resources :users, only: [:show, :index, :edit, :update] do
     member do
